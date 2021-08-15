19bcbeb93350f002cda820313b690a79292d1a33
yoshida <s1y1u1n1@yahoo.co.jp>
2021-08-15 23:01:00 +0900

    簡易メモアプリの作成(CRSF,XSS対策)

--- work/web/index.php
+++ work/web/index.php
@@ -1,13 +1,42 @@
 <?php
 
 require('../app/functions.php');  // 処理をとめる
+
+createToken();
+
+define('FILENAME', '../app/messages.txt');
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+  validateToken();
+  
+  $message = trim(filter_input(INPUT_POST, 'message'));
+  $message = $message !== '' ? $message : '...';
+  
+
+  $fp = fopen(FILENAME, 'a');
+  fwrite($fp, $message . "\n");
+  fclose($fp);
+
+  header('Location: http://localhost:8080/result.php');
+  exit;
+}
+
+$messages = file(FILENAME, FILE_IGNORE_NEW_LINES);
+
 include('../app/_parts/_header.php');   // 処理を止めない
 
 ?>
 
-<form action = "result.php" method="get">
-  <input type = "text" name = "message">
-  <button>Send</button>
+<ul>
+  <?php foreach($messages as $message): ?>
+    <li><?= h($message);?></li>
+  <?php endforeach; ?>
+</ul>
+
+<form action = "" method="post">
+  <input type="text" name="message">
+  <button>Post</button>
+  <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
 </form>
 
 
--- work/web/result.php
+++ work/web/result.php
@@ -2,14 +2,11 @@
 
 require('../app/functions.php');  // 処理をとめる
 
-$message = trim(filter_input(INPUT_GET, 'message'));
-$message = $message !== '' ? $message : '...';
-
 include('../app/_parts/_header.php');   // 処理を止めない
 
 ?>
 
-<p><?= h($message);?> </p>
+<p>message added</p>
 <p><a href="index.php">back</a></p>
 
 <?php
